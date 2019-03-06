d8c66e0bb1d8baabe20a52edb00325f3413c5aa8
Cameron Rison <cameron.rison@utexas.edu>
2019-03-05 22:07:10 -0600

    Updating for testing in Chef

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const port = 9000
 app.prepare()
     .then(() => {
         const server = express()
@@ -13,9 +14,9 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(3000, (err) => {
+        server.listen(port, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log('> Ready on http://localhost:' + port)
         })
     })
     .catch((ex) => {
